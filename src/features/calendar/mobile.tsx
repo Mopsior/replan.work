691bfef4b66f61185eef997fb194959375096575
Bartosz Sułkowski <kontakt@mopsior.pl>
2026-04-14 14:00:04 +0200

    feat: add isFree disclaimer
    - added reseting scroll on month change

--- src/features/calendar/mobile.tsx
+++ src/features/calendar/mobile.tsx
@@ -1,8 +1,9 @@
 import { useSearch } from '@tanstack/react-router'
-import { ChevronRight } from 'lucide-react'
+import { ChevronRight, CircleAlert } from 'lucide-react'
 import { useEffect, useRef } from 'react'
 import { Fragment } from 'react/jsx-runtime'
 import { useTranslation } from 'react-i18next'
+import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { useCalendar } from '@/hooks/use-calendar'
 import { useEvents } from '@/hooks/use-events'
@@ -27,14 +28,14 @@ export const MobileCalendar = () => {
         year,
     })
 
-    const { data: events, isLoading } = useEvents(userId, month, year)
+    const { data: events, isLoading, error } = useEvents(userId, month, year)
 
     const today = new Date()
     const currentWeekRef = useRef<HTMLDivElement>(null)
 
     const getCurrentWeekIndex = () => {
         if (today.getMonth() + 1 !== month || today.getFullYear() !== year) {
-            return -1
+            return 0
         }
         return Math.floor((today.getDate() + firstDayOfMonth - 2) / 7)
     }
@@ -49,6 +50,11 @@ export const MobileCalendar = () => {
             })
         }
     }, [month, year])
+
+    if (error) {
+        toast.error(t('calendar.event.fetchError'))
+        console.error(error)
+    }
 
     return (
         <div className='space-y-24 h-full w-full md:hidden scroll-smooth snap-mandatory snap-y pt-0 p-8 pb-16 overflow-y-auto scroll-pb-20'>
@@ -86,28 +92,36 @@ export const MobileCalendar = () => {
                                         className='space-y-2.5'
                                         key={`week-${weekIndex}-day-${day?.day}`}
                                     >
-                                        <div className='flex items-end gap-x-2'>
-                                            <h3
-                                                className={cn([
-                                                    'text-2xl',
-                                                    weekDay === 5 || weekDay === 6
-                                                        ? 'text-muted-foreground'
-                                                        : 'text-foreground',
-                                                    day.day === today.getDate() &&
-                                                        month === today.getMonth() + 1 &&
-                                                        year === today.getFullYear() && [
-                                                            'bg-primary relative',
-                                                            'before:absolute before:-left-1 before:top-0 before:w-1 before:h-full before:bg-primary before:rounded-l',
-                                                            'after:absolute after:-right-1 after:top-0 after:w-1 after:h-full after:bg-primary after:rounded-r',
-                                                            'text-background',
-                                                        ],
-                                                ])}
-                                            >
-                                                {day?.day}
-                                            </h3>
-                                            <p className='text-xl text-muted-foreground'>
-                                                {weekDayName}
-                                            </p>
+                                        <div className='flex justify-between items-end gap-x-2'>
+                                            <div className='flex gap-x-2 items-end'>
+                                                <h3
+                                                    className={cn([
+                                                        'text-2xl',
+                                                        weekDay === 5 || weekDay === 6
+                                                            ? 'text-muted-foreground'
+                                                            : 'text-foreground',
+                                                        day.day === today.getDate() &&
+                                                            month === today.getMonth() + 1 &&
+                                                            year === today.getFullYear() && [
+                                                                'bg-primary relative',
+                                                                'before:absolute before:-left-1 before:top-0 before:w-1 before:h-full before:bg-primary before:rounded-l',
+                                                                'after:absolute after:-right-1 after:top-0 after:w-1 after:h-full after:bg-primary after:rounded-r',
+                                                                'text-background',
+                                                            ],
+                                                    ])}
+                                                >
+                                                    {day?.day}
+                                                </h3>
+                                                <p className='text-xl text-muted-foreground'>
+                                                    {weekDayName}
+                                                </p>
+                                            </div>
+                                            {day.isFree && (
+                                                <div className='flex items-center gap-x-2 text-sm text-destructive pb-1'>
+                                                    <CircleAlert size={16} />
+                                                    <p>{t('calendar.event.freeDay')}</p>
+                                                </div>
+                                            )}
                                         </div>
                                         {eventsForDay && eventsForDay.length > 0
                                             ? eventsForDay.map((event) => {
